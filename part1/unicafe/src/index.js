ef3c63c6d480f565ec16036135b93ab81e85e249
dannch <deduardo155@gmail.com>
2020-10-19 07:12:22 -0500

    Final version

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood=()=>setGood(good +1)
+  const increaseNeutral=()=>setNeutral(neutral +1)
+  const increaseBad=()=>setBad(bad +1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={increaseGood}>
+        good
+      </button>
+      <button onClick={increaseNeutral}>
+        neutral
+      </button>
+      <button onClick={increaseBad}>
+        bad
+      </button>
+      <h1>statistics</h1>
+      <p>{"good      " + good}</p>
+      <p>{'neutral   ' + neutral}</p>
+      <p>{'bad       ' + bad}</p>
+      <p>{'all       ' + (good+neutral+bad)}</p>
+      <p>{'average   ' + (good+neutral+bad)/3}</p>
+      <p>{'positive  ' + (good)/(good+neutral+bad)*100+'%'}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
